22f778893c0eeedf0f68363a15d486018faa2a4b
wadsw053 <133675750+wadsw053@users.noreply.github.com>
2023-07-21 11:21:23 -0500

    Delete File conversion directory

--- File conversion/DM3read.m
+++
@@ -1,6 +0,0 @@
-function dm3image = DM3read(dm3filename)
-% DM3READ imports ONLY the image part of the .dm3 file (dm3filename) and 
-% outputs a array dm3image containing the pixel information
-
-DM3struct = DM3Import(dm3filename);
-dm3image = transpose(DM3struct.image_data);
